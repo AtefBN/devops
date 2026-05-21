2d3329ce43da736e4ec27e9aeef700550ca97be2
AtefBN <abennasser@ipsl.fr>
2026-05-21 23:41:39 +0200

    removed deprecated dependencies from constants.sh

--- installers/rocky/shared/sh/constants.sh
+++ installers/rocky/shared/sh/constants.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Imports.
-source /opt/esgf/devops/installers/ubuntu/shared/sh/constants.sh
-
 # Array of managed libraries.
 declare -a INSTALLER_REPOS=(
     'esdoc-errata-fe'
